8b857498b46abe671290be1d96685f860f129c12
kevinwtl <44014986+kevinwtl@users.noreply.github.com>
2020-07-24 13:06:23 +0800

    update

--- trigger.py
+++ trigger.py
@@ -7,12 +7,17 @@ import win32com.client as win32
 
 pd.options.display.float_format = "{:,.2f}".format
 
-df = pd.read_csv('CCASS_tracker' + os.sep + 'CCASS_database.csv')
+database = pd.read_csv('CCASS_tracker' + os.sep + 'CCASS_database.csv')
 
-last_data_date = sorted(list(df['Date'].unique()))[-1]
+last_data_date = sorted(list(database['Date'].unique()))[-1]
+
+outlook = win32.Dispatch('outlook.application')
+
+mail = outlook.CreateItem(0)
+
 
 # Rolling 15 days changes > 10%
-def rolling_days_changes(df,days = 15 ,threshold = 10):
+def rolling_days_changes(df, days = 15 ,threshold = 10):
     '''Calculate rolling days changes in total CCASS holdings.'''
     date_list = sorted(list(df['Date'].unique()))[-days:]
     filtered_df = df[df.Date.isin(date_list)]
@@ -43,11 +48,58 @@ def rolling_days_changes(df,days = 15 ,threshold = 10):
     else:
         pass
 
-outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
-mail.To = 'jameshan@chinasilveram.com;prashantgurung@chinasilveram.com'
-mail.Subject = 'CCASS major changes (up to ' + last_data_date + ')'
-my_html = r"<p>Dear Team,</p><p>&nbsp;</p><p>Here's the summary of the recent CCASS major changes (&gt;10% change in the past 15 trading days) for stocks we are monitoring.</p><p>&nbsp;</p>" + rolling_days_changes(df).to_html(index = False) + r"<p>* Denominator of the percentages is the number of all shares/warrants/units issued in total.</p><p>&nbsp;</p><p>Regards,</p><p>Kevin Wong</p>"
-mail.HTMLBody = my_html
-mail.Display(False)
-#mail.Send()
+
+def find_block_trader(df, days = 15 ,threshold = 10):
+    date_list = sorted(list(df['Date'].unique()))[-days:]
+    filtered_df = df[df.Date.isin(date_list)]
+    df1 = filtered_df.groupby(['Ticker','CCASS ID'])['DoD Change'].sum().reset_index()
+    df1.rename(columns = {'DoD Change':'Cumulative Change'}, inplace = True)
+    df1['Date'] = last_data_date
+    df1['Shareholding'] = np.nan
+    df1[r'% of Total Issued Shares/Warrants/Units'] = np.nan
+    df1['DoD Change'] = np.nan
+    df1 = df1.set_index(['Ticker','CCASS ID'])
+    df1.update(df[df['Date'] == date_list[-1]].set_index(['Ticker','CCASS ID']))
+    df1 = df1[abs(df1['Cumulative Change']) > threshold]
+
+    return df1.reset_index(drop = False)
+
+
+def sub_table(row, threshold_multiplier = 0.1):
+    ticker = row['Ticker']
+    CCASS_ID = row['CCASS ID']
+    threshold = row['Cumulative Change'] * threshold_multiplier
+
+    df = database.groupby(['Ticker','CCASS ID']).get_group((ticker,CCASS_ID)).loc[(abs(database['DoD Change'])>threshold)].reset_index(drop = True)
+
+    df = df.append(row)
+    df = df.set_index(['Ticker','CCASS ID','Date'])
+
+    df = df.sort_values(by = 'Date', ascending = True)
+
+    df = df.replace(np.nan, '', regex=True) # Replace nan by blank for better formatting
+
+    # Rename columns
+    df.columns = ['Shareholding', '% of Total Issued Shares/Warrants/Units *', 'DoD Change (%) *','Cumulative Change (%) *']
+
+    return df
+
+
+def create_mail_draft(df):
+    mail.To = 'jameshan@chinasilveram.com;prashantgurung@chinasilveram.com'
+    mail.Subject = 'CCASS major changes (up to ' + last_data_date + ')'
+    my_html = r"<p>Dear Team,</p><p>&nbsp;</p><p>Here's the summary of the recent CCASS major changes (&gt;10% change in the past 15 trading days) for stocks we are monitoring.</p><p>&nbsp;</p>" + df.to_html(index = True) + r"<p>* Denominator of the percentages is the number of all shares/warrants/units issued in total.</p><p>&nbsp;</p><p>Regards,</p><p>Kevin Wong</p>"
+    mail.HTMLBody = my_html
+    mail.Display(False)
+
+
+def main():
+    table = pd.DataFrame()
+    for i in find_block_trader(database).index:
+        table = table.append(sub_table(find_block_trader(database).iloc[i]).reset_index(drop = False))
+
+    table = table.set_index(['Ticker','CCASS ID','Date'])
+
+    create_mail_draft(table)
+
+    mail.Send()
